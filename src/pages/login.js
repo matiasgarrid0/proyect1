95612579ecff06668ec238bdc7e839038c91b1d7
Matias Garrido <matias17garrido@gmail.com>
2023-02-16 20:24:25 -0300

    starting

---
+++ src/pages/login.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Button from '@mui/material/Button';
+const login = () => {
+  return (
+    <div>login
+        <Button variant='contained'>
+            Hola
+        </Button>
+    </div>
+  )
+}
+
+export default login
